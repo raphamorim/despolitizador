ec3fae7765186b2ae184c7ee4029620dcf02d5ad
Luís Felipe Souza <luisfmsouza@gmail.com>
2018-08-07 12:11:04 -0300

    Use the json library to test the code

--- src/js/__tests__/translator.js
+++ src/js/__tests__/translator.js
@@ -1,28 +1,20 @@
 import { translate } from '../translator';
+import library from '../../library.json';
 
 describe('translate', () => {
   test('the input with a single word', () => {
-    const library = { 'vossa excelencia': 'mano' };
     const inputText = 'vossa excelencia';
 
     expect(translate(library, inputText)).toBe('mano');
   });
 
   test('the input with multiple words', () => {
-    const library = {
-      'vossa excelencia': 'mano',
-      'estadista': 'membro da casta mais alta'
-    };
-    const inputText = 'vossa excelencia é um estadista';
+    const inputText = 'vossa excelência é um estadista';
 
     expect(translate(library, inputText)).toBe('mano é um membro da casta mais alta');
   });
 
   test('the input with accent words', () => {
-    const library = {
-      'vossa excelência': 'mano',
-      'estadista': 'membro da casta mais alta'
-    };
     const inputText = 'vossa excelência é um estadista';
 
     expect(translate(library, inputText)).toBe('mano é um membro da casta mais alta');
